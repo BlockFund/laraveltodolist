8a75627715630be8a38a65eb76b8841022e35c05
Elias Baez <elias@blockfundcrypto.com>
2020-12-18 13:26:10 -0600

    Edit welcome message

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 
         <div class="container py-5">
             <header class="text-center text-black py-5">
-                <h1 class="display-4 font-weight-bold mb-4">Welcome Stranger!</h1>
+                <h1 class="display-4 font-weight-bold mb-4">Welcome to my demo!</h1>
                 </p>
             </header>
         </div>
